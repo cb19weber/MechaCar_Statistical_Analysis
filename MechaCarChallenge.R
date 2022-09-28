b343776de28add0627f5d98c7c64b38c08f50f3d
cb19weber <webercullen@gmail.com>
2022-09-27 23:28:03 -0600

    deliverable 1 complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,13 +9,10 @@ mpg_matrix <- as.matrix(vehicle_mpg)
 cor(mpg_matrix)
 
 # view linear model
-lm(mpg ~ vehicle_length, vehicle_mpg)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=vehicle_mpg)
 
 # store model and built ggplot
-mpg_model <- lm(mpg ~ vehicle_length, vehicle_mpg)
-yvals <- mpg_model$coefficients['vehicle_length']*vehicle_mpg$vehicle_length + mpg_model$coefficients['(Intercept)']
-plt <- ggplot(vehicle_mpg,aes(x=vehicle_length,y=mpg))
-plt + geom_point() + geom_line(aes(y=yvals), color = 'red')
+mpg_model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=vehicle_mpg)
 
 # 6. Determine the p-value and the r-squared value for the linear regression model
 summary(mpg_model)
